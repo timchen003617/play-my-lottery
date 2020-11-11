13b432c67b0acd973d6a4625f8519d35d31e8b61
timchen <gn02321555@gmail.com>
2020-11-11 19:43:12 +0800

    test

--- client/webpack.common.js
+++ client/webpack.common.js
@@ -25,7 +25,7 @@ module.exports = {
   resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
     path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
+    publicPath: "/",
     filename: "main.js",
   },
   plugins: [new HtmlWebpackPlugin(), new webpack.HotModuleReplacementPlugin()],
